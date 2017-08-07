9796bc07329ae00d6281f0497777c2f044adc161
Adam Smith <asmith13194@gmail.com>
2017-08-06 18:29:38 -0600

    create component structure

---
+++ src/components/Ads.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Ads = ( ) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Ads
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = ( ) => {
+
+  return (
+    <nav>
+      <div className="nav-wrapper amber darken-2">
+        <div className="col s12">
+          <a href="" className="brand-logo left">g[Classifieds]</a>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
